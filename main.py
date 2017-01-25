4b452cb88dd26ba81418c30477701b8f173cee8e
churakaagii <a.r.nakama@gmail.com>
2017-01-25 12:53:24 -0500

    now completely extendable, and cleaner

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import webapp2, re
 
-UNAME_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-PW_RE = re.compile(r"^.{3,20}$")
-EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
-# list of regex to use
+REGEX_DICT = {
+    "username": re.compile(r"^[a-zA-Z0-9_-]{3,20}$"),
+    "password": re.compile(r"^.{3,20}$"),
+    "email": re.compile(r"^[\S]+@[\S]+.[\S]+$")
+} # dictionary of regex to use, must be extended
 
 class Handler(webapp2.RequestHandler): #helper class
     def write(self, *args, **kwargs): 
@@ -25,14 +26,12 @@ class MainHandler(Handler):
         totalpage = header
         
         inputelem = (
-            ("username", "Username", "text", kwargs.get("uname_err", ""), 
-                kwargs.get("uname_init", "")), 
-            ("password", "Password", "password", kwargs.get("pw_err", ""), ""), 
+            ("username", "Username", "text", kwargs.get("username_err", ""), kwargs.get("username_init", "")), 
+            ("password", "Password", "password", kwargs.get("password_err", ""), ""), 
             ("verify", "Re-enter Password", "password", kwargs.get("v_err", ""), ""),
-            ("email", "Email (optional)", "text", kwargs.get("email_err", ""), 
-                kwargs.get("email_init",""))
-        ) # update this to get more form fields 
-        # format is: (field name, label text, field type, error message, default text)
+            ("email", "Email (optional)", "text", kwargs.get("email_err", ""), kwargs.get("email_init",""))
+        ) # a set of variables to initialize form fields, must be extended
+        # format is: (field name, label text, field type, error message variable, default text)
         
         for element in inputelem:
             totalpage += self.build_input(*element)
@@ -59,74 +58,55 @@ class MainHandler(Handler):
         self.write(self.build_form())
         
     def post(self):
-        checklist = {} # initialize passed values, update if more
-        username = self.request.get("username")
-        pw = self.request.get("password")
-        verify = self.request.get("verify")
-        email = self.request.get("email")
+        checklist = {} # will be filled with validated user input
+        checklist_init = [] # will be passed into checklist constructor
+        passed_dict = self.request.POST # user input
+        prep_validation = list(filter((lambda x: not(x == "verify" or x == "email")), passed_dict.iterkeys())) # list of all input to be validated
         
-        checklist_init = [
-            ("uname", username, UNAME_RE), 
-            ("pw", pw, PW_RE)
-        ] # list of all strings to by checked by regex
-        # format is (checklist key, user data from field, regex to use)
+        for key in prep_validation: # populated list for checklist constructor
+            checklist_init.append((key, passed_dict[key], REGEX_DICT[key]))
         
-        if email: # checks if email exists
-            checklist_init.append(("email", email, EMAIL_RE))
+        if passed_dict["email"]: # checks if email exists
+            checklist_init.append(("email", passed_dict["email"], REGEX_DICT["email"]))
         else:
-            checklist["email_valid"] = True
+            checklist["valid email"] = True
         
-        for checkset in checklist_init: # builds dictionary of validated user input
-            checklist["%s_valid"%checkset[0]] = self.check_valid(checkset[1], checkset[2])
+        for checkset in checklist_init: # constructs checklist
+            checklist["valid %s"%checkset[0]] = self.check_valid(checkset[1], checkset[2])
         
-        if verify == pw: # checks if pw and re-entered pw match
-            checklist["v_valid"] = True
+        if passed_dict["verify"] == passed_dict["password"]: # checks if pws match
+            checklist["matching password"] = True
         else:
-            checklist["v_valid"] = False
+            checklist["matching password"] = False
         
         if all(checklist.itervalues()):
-            self.redirect("/welcome?username="+username) # redirect on success
-        else:
-            if not checklist["uname_valid"]:
-                uname_err = "Please enter a valid username (you can use letters, numbers, underscore or dash)"
-            else:
-                uname_err = ""
-                
-            if not checklist["pw_valid"]:
-                pw_err = "Please enter a valid password of at least 3 characters"
-            else:
-                pw_err = ""
-                
-            if not checklist["v_valid"]:
-                v_err = "Passwords do not match"
-            else:
-                v_err = ""
-                
-            if not checklist["email_valid"]:
-                email_err = "Please enter a valid email"
-            else:
-                email_err = ""
-                
-            self.write(self.build_form(
-                uname_err = uname_err,
-                pw_err = pw_err,
-                v_err = v_err,
-                email_err = email_err, 
-                uname_init = username, 
-                email_init = email
-            )) # builds error page, make sure to pass error message and initial values
+            self.redirect("/welcome?username="+passed_dict["username"]) # redirect on success
+        else: # otherwise generate error messages and build a new form 
+            errordict = {} # will be populated with errors to show to user
+            
+            for key in checklist.iterkeys(): # constructs errordict
+                if not checklist[key]: # if input not valid, adds error
+                    if key == "matching password": # unique check for matching pw
+                        key_name = "v_err"
+                    else:
+                        var_name = key.partition("valid ")
+                        key_name = var_name[2] + "_err"
+                        
+                    errordict[key_name] = "Please enter a %s" % key
+            
+            self.write(self.build_form(**errordict)) # builds error page
 
 class WelcomeHandler(Handler):
     def build_welcome(self, username): # builds welcome page
         welc_html = """
-            <h1>Welcome, %s</h1>
+            <h1>Welcome %s</h1>
         """
         return welc_html % username
     
     def get(self):
         username = self.request.get("username")
         if username.lower() == "ada": # v important
-            username = "magnificent princess Ada, who must be obeyed in all things"
+            username = "international space princess Ada, who must be obeyed in all things"
             
         self.write(self.build_welcome(username))
     
