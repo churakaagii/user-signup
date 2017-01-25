4eaa5a4898d1268a7c5f501e3012b73f9a7fe6f0
churakaagii <a.r.nakama@gmail.com>
2017-01-25 00:43:30 -0500

    complete build

--- main.py
+++ main.py
@@ -21,32 +21,34 @@ class MainHandler(Handler):
         footer = """
                 <input type='submit'>
             </form>
+            <p style="font-size: .8em;">coded by ada nakama</p>
         """
         
         totalpage = header
         
         inputelem = (
-            ("username", "Username", kwargs.get("uname_err", "")), 
-            ("password", "Password", kwargs.get("pw_err", "")), 
-            ("verify", "Re-enter Password", kwargs.get("ver_err", "")),
-            ("email", "Email (optional)", kwargs.get("email_err", ""))
+            ("username", "Username", "text", kwargs.get("uname_err", ""), kwargs.get("uname_init", "")), 
+            ("password", "Password", "password", kwargs.get("pw_err", ""), ""), 
+            ("verify", "Re-enter Password", "password", kwargs.get("v_err", ""), ""),
+            ("email", "Email (optional)", "text", kwargs.get("email_err", ""), kwargs.get("email_init",""))
         )
         
         for element in inputelem:
-            totalpage += self.build_input(element[0], element[1], element[2])
+            totalpage += self.build_input(element[0], element[1], element[2], element[3], element[4])
             
         totalpage += footer
 
         return totalpage
         
-    def build_input(self, internal, external, error=""):
+    def build_input(self, internal, external, thistype="text", error="", init_value=""):
         htmlused = """
         <label for='%(internal)s'>%(external)s: </label>
-        <br/><input type='text' name='%(internal)s'> %(error)s
+        <br/><input type='%(thistype)s' name='%(internal)s' value='%(init_value)s'> 
+        <span style='color: red;'>%(error)s</span>
         <br/>
         <br/>
         """
-        return htmlused % {"internal": internal, "external": external, "error": error}
+        return htmlused % {"internal": internal, "external": external, "error": error, "thistype": thistype, "init_value": init_value}
     
     def check_valid(self, string, regex):
         return regex.match(string)
@@ -68,6 +70,8 @@ class MainHandler(Handler):
         
         if email:
             checklist_init.append(("email", email, EMAIL_RE))
+        else:
+            checklist["email_valid"] = True
         
         for checkset in checklist_init:
             checklist["%s_valid"%checkset[0]] = self.check_valid(checkset[1], checkset[2])
@@ -80,12 +84,39 @@ class MainHandler(Handler):
         if all(checklist.itervalues()):
             self.redirect("/welcome?username="+username)
         else:
-            self.write("butts")
+            if not checklist["uname_valid"]:
+                uname_err = "Please enter a valid username"
+            else:
+                uname_err = ""
+                
+            if not checklist["pw_valid"]:
+                pw_err = "Please enter a valid password"
+            else:
+                pw_err = ""
+                
+            if not checklist["v_valid"]:
+                v_err = "Passwords do not match"
+            else:
+                v_err = ""
+                
+            if not checklist["email_valid"]:
+                email_err = "Please enter a valid email"
+            else:
+                email_err = ""
+                
+            self.write(self.build_form(
+                uname_err = uname_err,
+                pw_err = pw_err,
+                v_err = v_err,
+                email_err = email_err, 
+                uname_init = username, 
+                email_init = email
+            ))
 
 class WelcomeHandler(Handler):
     def build_welcome(self, username):
         welc_html = """
-            Welcome, %s
+            <h1>Welcome, %s</h1>
         """
         return welc_html % username
     
